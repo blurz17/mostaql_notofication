ce5ead19697d480affff8eeacdccfeb376cc4ca3
John7Lj <ffkiuyh@gmail.com>
2026-09-13 23:05:14 +0300

    Add do_HEAD to dummy server for cron-job.org compatibility

--- main.py
+++ main.py
@@ -165,6 +165,11 @@ class DummyHandler(BaseHTTPRequestHandler):
         self.send_header('Content-type', 'text/html')
         self.end_headers()
         self.wfile.write(b"Bot is active and running 24/7!")
+        
+    def do_HEAD(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
 
 def start_dummy_server():
     port = int(os.environ.get("PORT", 8080))
